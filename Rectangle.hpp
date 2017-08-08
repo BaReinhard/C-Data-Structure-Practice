88d1bd15d76416e389060a15011182a906f146e7
Brett Reinhard <brett@reinhards.us>
2017-08-08 09:41:28 -0700

    Added DrawRect(), to draw the rectangle in the command prompt

--- Rectangle.hpp
+++ Rectangle.hpp
@@ -1,6 +1,9 @@
 #ifndef RECTANGLE_HPP
 #define RECTANGLE_HPP
-
+#include <iostream>
+#include <string>
+using std::cout;
+using std::string;
 class Rectangle{
 
     public:
@@ -11,6 +14,23 @@ class Rectangle{
     double Height() const {return _height;}
     double Area() const {return _height*_width;}
     double Perimeter() const {return 2.0*(_height+_width);}
+    void DrawRect() const{
+        string topBottomLine =".";
+        string middleChunk="|";
+        for(int i=0;i<_width;i++){
+            topBottomLine+="--";
+            if(i!= 0){
+                middleChunk+="  ";
+            }
+        }
+        topBottomLine+=".";
+        middleChunk+="  |";
+        cout << topBottomLine << "\n";
+        for(int m =0;m<_height;m++){
+            cout << middleChunk << "\n";
+        }
+        cout << topBottomLine<<"\n";
+    }
     void Scale(double scaleFactor){
         _width *= scaleFactor;
         _height*=scaleFactor;
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "Rectangle.hpp"
+#include <string>
 using std::cout;
+using std::string;
 
 // Easy Print Info function
 void PrintInfo(const Rectangle& r){
@@ -8,24 +10,29 @@ void PrintInfo(const Rectangle& r){
          <<", height = " << r.Height()
          <<".\n" 
          <<"area = " << r.Area()
-         <<"Perimeter = " << r.Perimeter()
+         <<", Perimeter = " << r.Perimeter()
          <<"\n\n";
 }
+
 int main(){
-    Rectangle r1{};
-    Rectangle r2{20,40};
+    
+    Rectangle r1{1,1};
+    r1.DrawRect();
+    Rectangle r2{2,2};
+    r2.DrawRect();
     PrintInfo(r1);
     cout << "\n\n";
     PrintInfo(r2);
     r2.Scale(2);
+    r2.DrawRect();
     cout << "\n\n";
     PrintInfo(r2);
-    r2.Scale(0.5);
+    r2.Scale(2.5);
+    r2.DrawRect();
     cout << "\n\n";
     
     
     PrintInfo(r2);
-    
    
 }
 
