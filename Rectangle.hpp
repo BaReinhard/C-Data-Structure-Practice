c8794c02080bbed8450e57e5996ddfaa257bb604
Brett Reinhard <brett@reinhards.us>
2017-08-09 10:32:57 -0700

    Added Diagonal Length, Inner Triangle Area and Perimeter, and Fixed some logic by casting a double to an int for comparison and for loops

--- Rectangle.hpp
+++ Rectangle.hpp
@@ -2,41 +2,84 @@
 #define RECTANGLE_HPP
 #include <iostream>
 #include <string>
+#include <math.h>
 using std::cout;
 using std::string;
-class Rectangle{
 
-    public:
-    Rectangle()= default;
-    Rectangle(double width,double height)
-    : _width{width} , _height{height}{}
-    double Width() const {return _width;}
-    double Height() const {return _height;}
-    double Area() const {return _height*_width;}
-    double Perimeter() const {return 2.0*(_height+_width);}
-    void DrawRect() const{
-        string topBottomLine =".";
-        string middleChunk="|";
-        for(int i=0;i<_width;i++){
-            topBottomLine+="--";
-            if(i!= 0){
-                middleChunk+="  ";
+// Create a Rectangle Class
+class Rectangle
+{
+    // Public Methods
+  public:
+    // Default Constructor, allows for init with no given params, C++11 feature
+    Rectangle() = default;
+    // Constructor Method, inits private variables
+    Rectangle(double width, double height)
+        : _width{width}, _height{height} {}
+    // Returns the private variable _width, no internal variable change, const
+    double Width() const { return _width; }
+    // Returns the private variable _height, no internal variable change, const
+    double Height() const { return _height; }
+    // Calculates and Returns the Area of th Rectangle, no internal variable change, const
+    double Area() const { return _height * _width; }
+    // Calculates and Returns the Perimeter of the Rectangle, no internal variable change, const
+    double Perimeter() const { return 2.0 * (_height + _width); }
+    // Calculates and Returns the Diagonal Length of the Rectangle, no internal variable change, const
+    double DiagonalLength() const { return sqrt(pow(_height, 2.0) + pow(_width, 2.0)); }
+    void ConsoleInnerTriangles() const
+    {
+        cout << "Triangle 1 & 3: Area: " << triangleArea(_height, _width * 0.5)
+             << ", Perimeter: " << trianglePerimeter(_height, DiagonalLength() * 0.5)
+             << "\nTriangle 2 & 4: Area: " << triangleArea(_width, _height * 0.5)
+             << ", Perimeter: " << trianglePerimeter(_width, DiagonalLength() * 0.5)
+             << "\n\n";
+    }
+    // Draws a Rectangle in the Command Line, also doesn't change internal variables, thus the const keyword
+    void DrawRect() const
+    {
+        if ((int)_width < 40)
+        {
+            string topBottomLine = ".";
+            string middleChunk = "|";
+            for (int i = 0; i < (int)_width; i++)
+            {
+                topBottomLine += "--";
+                if (i != 0)
+                {
+                    middleChunk += "  ";
+                }
             }
+            topBottomLine += ".";
+            middleChunk += "  |";
+            cout << topBottomLine << "\n";
+            for (int m = 0; m < (int)_height; m++)
+            {
+                cout << middleChunk << "\n";
+            }
+            cout << topBottomLine << "\n";
         }
-        topBottomLine+=".";
-        middleChunk+="  |";
-        cout << topBottomLine << "\n";
-        for(int m =0;m<_height;m++){
-            cout << middleChunk << "\n";
+        else
+        {
+            cout << "Rectangle is too large to print on screen";
         }
-        cout << topBottomLine<<"\n";
     }
-    void Scale(double scaleFactor){
+    // Scale changes internal variables, thus it is not constant
+    void Scale(double scaleFactor)
+    {
         _width *= scaleFactor;
-        _height*=scaleFactor;
+        _height *= scaleFactor;
     }
-    private:
-    double _width{};    
+    // Private Variables
+  private:
+    double _width{};
     double _height{};
+    double triangleArea(double base, double height) const
+    {
+        return (0.5 * base * height);
+    }
+    double trianglePerimeter(double base, double side) const
+    {
+        return (base + side + side);
+    }
 };
 #endif
--- main.cpp
+++ main.cpp
@@ -5,20 +5,24 @@ using std::cout;
 using std::string;
 
 // Easy Print Info function
-void PrintInfo(const Rectangle& r){
+void PrintInfo(const Rectangle &r)
+{
     cout << "Rectanle width = " << r.Width()
-         <<", height = " << r.Height()
-         <<".\n" 
-         <<"area = " << r.Area()
-         <<", Perimeter = " << r.Perimeter()
-         <<"\n\n";
+         << ", height = " << r.Height()
+         << ".\n"
+         << "area = " << r.Area()
+         << ", Perimeter = " << r.Perimeter()
+         << "\n Diagonal Length = " << r.DiagonalLength()
+         << "\n\n";
+    r.ConsoleInnerTriangles();
 }
 
-int main(){
-    
-    Rectangle r1{1,1};
+int main()
+{
+
+    Rectangle r1{1, 4};
     r1.DrawRect();
-    Rectangle r2{2,2};
+    Rectangle r2{30, 20};
     r2.DrawRect();
     PrintInfo(r1);
     cout << "\n\n";
@@ -29,11 +33,7 @@ int main(){
     PrintInfo(r2);
     r2.Scale(2.5);
     r2.DrawRect();
+    r2.DiagonalLength();
     cout << "\n\n";
-    
-    
     PrintInfo(r2);
-   
 }
-
-
